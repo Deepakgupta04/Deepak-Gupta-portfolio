d219605f341ec15d6d0036aea43016d8d389dc05
Deepak Gupta <89908558+Deepakgupta04@users.noreply.github.com>
2022-10-14 22:51:24 +0530

    Update pf.js

--- pf.js
+++ pf.js
@@ -34,18 +34,18 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{});
-sr.reveal('.button',{delay: 200});
-sr.reveal('.home__img',{delay: 200});
-sr.reveal('.home__social-icon',{ interval: 200});
+sr.reveal('.button',{delay: 100});
+sr.reveal('.home__img',{delay: 100});
+sr.reveal('.home__social-icon',{ interval: 100});
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{});
-sr.reveal('.about__subtitle',{delay: 200});
-sr.reveal('.about__text',{delay: 200});
+sr.reveal('.about__subtitle',{delay: 100});
+sr.reveal('.about__text',{delay: 100});
 
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 200})
+sr.reveal('.skills__img', {delay: 100})
 
 /*SCROLL WORK*/
 sr.reveal('.portfolio__img', {interval: 100})
